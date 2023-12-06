2b3e75896a874bef09015e874a799524861bdae0
LukasZdrazilek <147374351+LukasZdrazilek@users.noreply.github.com>
2023-12-06 21:58:49 +0100

    Update ukolu

--- Vyuka_Zdrazilek_22-5KB-C/Othello_regularni_vyrazy/parsing_othelo.py
+++ Vyuka_Zdrazilek_22-5KB-C/Othello_regularni_vyrazy/parsing_othelo.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from os.path import join, realpath, dirname, exists
+import re
 
 if not exists(join(dirname(realpath(__file__)), "Othello.txt")):
     resp = requests.get("https://www.gutenberg.org/cache/epub/2267/pg2267.txt")
@@ -17,8 +18,7 @@ with open(join(dirname(realpath(__file__)), "Othello.txt"), "r") as file:
     slova_unikat = {slovo:slova.count(slovo) for slovo in set(slova)}
     print(sorted(slova_unikat.items(), key = lambda x : x[1], reverse = True)[:10])
     
-    
-    vety = re.findall(r"[^.!?]+[.!?]", book)
-    average_length = sum(len(veta) for veta in vety) / len(vety)
-    print("Pocet vet:", len(vety))
-    print("Prumerna delka vety:", average_length)
+    vety = re.findall(r"[A-Z][^.!?]*[.!?]", book)
+    validni_vety = [sentence for sentence in vety if len(sentence.split()) > 1]
+    pocet_vet = len(validni_vety)
+    print("Pocet vet:", pocet_vet)
